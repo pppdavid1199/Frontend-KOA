a0a36115caaf970e26ca34b90596f0f2cd638b49
pppdavid1199 <sz4papdav@vasvari.org>
2025-09-08 10:27:16 +0200

    Add files via upload

---
+++ Első feladat/script.js
@@ -0,0 +1,12 @@
+function ir()
+{
+    let nev = document.getElementById("nev").value;
+    let telepules = document.getElementById("telepules").value;;
+    let irszam = document.getElementById("irszam").value;;
+    let kozterulet = document.getElementById("kozterulet").value;;
+    let hazszam = document.getElementById("hazszam").value;;    
+
+    let kiiras;
+
+    kiiras = console.log(`Név: ${nev}, Település: ${telepules}, Irányítószám: ${irszam}, Közterület: ${kozterulet}, Házszám: ${hazszam}`)
+}
